8e308f623a46b155393b2734fe4afb20ff67f256
humbl3LilaS <sabishinekobebe@gmail.com>
2024-10-01 15:17:32 +0630

    add: constant for content rendering and type definitation file for ts support

---
+++ src/constants/constant.types.ts
@@ -0,0 +1,57 @@
+export interface TNavigation {
+	id: string;
+	title: string;
+	url: string;
+	onlyMobile?: boolean;
+}
+
+export type TStatus = "done" | "progress";
+
+export interface TRoadmap {
+	id: string;
+	title: string;
+	text: string;
+	date: string;
+	status: TStatus;
+	imageUrl: string;
+	colorful?: boolean;
+}
+
+export interface TColabContent {
+	id: string;
+	title: string;
+	text?: string;
+}
+
+export interface TColabApp {
+	id: string;
+	title: string;
+	icon: string;
+	width: number;
+	height: number;
+}
+
+export interface TPricing {
+	id: string;
+	title: string;
+	description: string;
+	price: string | null;
+	features: string[];
+}
+
+export interface TBenefit {
+	id: string;
+	title: string;
+	text: string;
+	backgroundUrl: string;
+	imageUrl: string;
+	iconUrl: string;
+	light?: boolean;
+}
+
+export interface TSocial {
+	id: string;
+	title: string;
+	iconUrl: string;
+	url: string;
+}
